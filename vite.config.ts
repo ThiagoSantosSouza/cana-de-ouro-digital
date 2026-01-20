457c680752e76f3ba53690e2d7c8a5ffe8890bf8
ThiagoSantos-Atom <thiagosantos@hackatom.space>
2026-01-20 16:03:09 -0300

    Add allowedhosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: true,
     host: "0.0.0.0",
     port: 5174,
     hmr: {
